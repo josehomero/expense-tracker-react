1afe43fd87cc10f325be71c75a7d007931b11bbf
josehomero <jose.homero.rios@outlook.com>
2020-08-03 20:09:55 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -8,37 +8,76 @@ import Table from './components/Table';
 class App extends React.Component {
   constructor() {
     super()
-    this.state= {form: {
-      type: "",
-      date: "",
-      description: "",
-      amount: ""
-    }}
+    this.state = {
+      form: {
+        type: "",
+        date: "",
+        description: "",
+        amount: ""
+      }
+    }
   }
 
   submitted() {
-    console.log("submitted")
+console.log(this.changeAmount, this.changeDate, this.changeDescription, this.changeType)
   }
 
+  changeDescription = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        description: e.target.value,
+      }
+    })
+  }
+
+  changeType = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        type: e.target.value
+      }
+    })
+  }
+  
+  changeDate = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        date: e.target.value
+      }
+    })
+  }
+
+  changeAmount = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        amount: e.target.value
+      }
+    })
+  }
 
   render() {
+
+
     return (
       <div className="App">
         <Header />
 
         <form>
-          <select name="type" value={this.props.type} onChange={this.props.handleChange} >
+          <select name="type" value={this.state.form.type} onChange={this.changeType} >
             <option value=''>Type</option>
             <option value="card">Card</option>
             <option value="cash">Cash</option>
             <option value="other">Other</option>
           </select>
-          <input name="date" type="text" placeholder="Date?" value={this.props.date} onChange={this.props.handleChange} />
-          <input name="description" type="text" placeholder="Where?" value={this.props.description} onChange={this.props.handleChange} />
-          <input name="amount" type="text" placeholder="Amount?" value={this.props.amount} onChange={this.props.handleChange} />
+          <input name="date" type="text" placeholder="Date?" value={this.state.form.date} onChange={this.changeDate} />
+          <input name="description" type="text" placeholder="Where?" value={this.state.form.description} onChange={this.changeDescription} />
+          <input name="amount" type="text" placeholder="Amount?" value={this.state.form.amount} onChange={this.changeAmount} />
         </form>
 
-        <button onClick={this.submitted}>Submit</button>
+        <button onChange={this.submitted}>Submit</button>
         <Table />
       </div>
     )
